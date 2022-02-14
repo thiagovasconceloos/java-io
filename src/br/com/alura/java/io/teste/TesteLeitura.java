61adc47a61dd31d117c400ccb9ab769a56f4d58a
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-02-14 13:29:35 -0300

    Iniciando o curso

---
+++ src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -0,0 +1,14 @@
+package br.com.alura.java.io.teste;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class TesteLeitura {
+
+	public static void main(String[] args) throws FileNotFoundException {
+	
+		FileInputStream fis = new FileInputStream("lorem.txt");
+       
+	}
+
+}
