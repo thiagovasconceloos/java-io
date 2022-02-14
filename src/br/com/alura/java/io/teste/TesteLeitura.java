6e6c5472750628f9412517d5657a1e95c861c07b
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-02-14 14:09:21 -0300

    Input File

--- src/br/com/alura/java/io/teste/TesteLeitura.java
+++ src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -1,14 +1,32 @@
 package br.com.alura.java.io.teste;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
+
 
 public class TesteLeitura {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException  {
+		//lendo o arquivo 
+		FileReader fr = new FileReader("lorem.txt");
+		// convertendo em caracteres.
+        BufferedReader br = new BufferedReader(fr);
+        
+        String linha = br.readLine();
+
+        while(linha != null) {
+                System.out.println(linha);
+                linha = br.readLine();
+        }
+
+        System.out.println(linha);
+
+        br.close();
+      
+
+
 	
-		FileInputStream fis = new FileInputStream("lorem.txt");
-       
+        
+        
 	}
 
 }
